ed8d9cc9c14a8da83c0f04afe0d4aff80c305df2
Marco Salvi <62235561+marcosalvi-01@users.noreply.github.com>
2025-05-10 18:13:31 +0200

    rounded borders

--- scripts/open.sh
+++ scripts/open.sh
@@ -41,6 +41,7 @@ if [ "$split_direction" = "p" ]; then
 			-h "${popup_height:-${popup_width}}" \
 			-x "${popup_x}" \
 			-y "${popup_y:-$popup_x}" \
+            -b rounded \
 			$extra_options \
 			-E "${extrakto} ${pane_id} popup"
 		rc=$?
